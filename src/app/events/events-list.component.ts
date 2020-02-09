4ae14636c57c9eed07f7fcbb4e5e95df6e3d1d66
hjstam1962 <hansjanke@outlook.com>
2020-02-09 13:12:57 +0100

    Tot aan toastr

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
+declare let toastr
+
 @Component({
     selector: 'events-list',
     template: `
@@ -8,7 +10,7 @@ import { EventService } from './shared/event.service';
     <hr>
     <div class="row">
         <div *ngFor="let event of events" class="col-md-5">
-            <event-thumbnail [event]="event"  ></event-thumbnail>
+            <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"  ></event-thumbnail>
         </div>
     </div>     
     `
@@ -23,6 +25,8 @@ export class EventsListComponent implements OnInit {
         this.events = this.eventService.getEvents();
     }
 
-    
+    handleThumbnailClick(eventName) {
+        toastr.success(eventName)
+    }
 
 }
