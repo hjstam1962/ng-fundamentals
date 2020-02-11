804caa6b42ca4d72b6b7140e9f75ab7e4fab71d9
hjstam1962 <hansjanke@outlook.com>
2020-02-11 22:16:57 +0100

    Chapter 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service'
 import { fromEventPattern } from 'rxjs';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 
 @NgModule({
   imports: [
@@ -33,6 +37,7 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     EventService, 
     ToastrService, 
     EventRouteActivator,
+    EventListResolver,
     { 
       provide: 'canDeactivateCreateEvent', 
       useValue: checkDirtyState 
