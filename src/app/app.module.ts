c2a458c2dd5745c3ed43481eb44d2948e61c6a83
hjstam1962 <hansjanke@outlook.com>
2020-02-25 21:40:24 +0100

    Search sessions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,11 +19,13 @@ import {
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { fromEventPattern } from 'rxjs';
-import { ToastrService } from './common/toastr.service';
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 import { CollapsibleWellComponent } from './common/collapsible-well.component';
+
+declare let toastr:Toastr;
 
 @NgModule({
   imports: [
@@ -47,7 +49,7 @@ import { CollapsibleWellComponent } from './common/collapsible-well.component';
   ],
   providers: [
     EventService, 
-    ToastrService, 
+    { provide: TOASTR_TOKEN, useValue: toastr}, 
     EventRouteActivator,
     EventListResolver,
     AuthService,
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service'
+import { ISession } from '../events/shared/event.model'
+import { EventService } from '../events/index'
 
 @Component({
     selector: 'nav-bar',
@@ -13,8 +15,17 @@ import { AuthService } from '../user/auth.service'
 })
 
 export class NavBarComponent implements OnInit {
-    constructor(private authService:AuthService) { }
+    searchTerm: string = "";
+    foundSessions: ISession[];
+    constructor(private authService:AuthService, private eventService:EventService) { }
 
     ngOnInit() { }
     
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm).subscribe
+            (sessions => {
+                this.foundSessions = sessions;
+                console.log(this.foundSessions);
+        })
+    }
 }
